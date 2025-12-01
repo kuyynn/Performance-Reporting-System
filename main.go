97becea18605bf29de1a229f65a2500970f7c6c8
kuyynn <indahtasyakurina@gmail.com>
2025-12-01 15:04:07 +0700

    project

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	// "database/sql"
+	"log"
+	"uas/app/repository"
+	"uas/app/service"
+	"uas/config"
+	"uas/middleware"
+	routes "uas/route"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-    fmt.Println("Hello World from Go!")
-};
+	cfg := config.LoadConfig()
+
+	log.Println("DSN:", cfg.PostgresDSN)
+
+	db, err := config.ConnectPostgres(cfg.PostgresDSN)
+	if err != nil {
+		log.Fatalf("pg connect: %v", err)
+	}
+	defer db.Close()
+
+	// init repos & services
+	userRepo := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepo)
+	authService := service.NewAuthService(userRepo)
+
+	app := fiber.New()
+
+	// routes: combine user & auth
+	routes.SetupRoutes(app, userService, authService, middleware.AuthRequired([]byte(cfg.JWTSecret)))
+
+	log.Fatal(app.Listen(":" + cfg.AppPort))
+}
