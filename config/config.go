05f96170a4c4a9bbd4c32d04d728b93ac8a591f3
kuyynn <indahtasyakurina@gmail.com>
2025-12-06 19:52:49 +0700

    project

--- config/config.go
+++ config/config.go
@@ -3,25 +3,27 @@ package config
 import (
 	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/caarlos0/env/v9"
 )
 
 type Config struct {
-	AppPort     string `env:"APP_PORT" envDefault:"8080"`
+	AppPort     string `env:"APP_PORT" envDefault:"3000"`
 	PostgresDSN string `env:"POSTGRES_DSN"`
 	MongoURI    string `env:"MONGO_URI" envDefault:"mongodb://localhost:27017"`
-	MongoDB     string `env:"MONGO_DB" envDefault:"prestasi_db"`
+	MongoDB     string `env:"MONGO_DB" envDefault:"uas_db"`
 	JWTSecret   string `env:"JWT_SECRET" envDefault:"changeme"`
 }
 
 func LoadConfig() *Config {
-	cfg := Config{}
-	if err := env.Parse(&cfg); err != nil {
-		log.Fatalf("Failed to load env: %v", err)
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("WARNING: .env file not found or failed to load")
 	}
 
-	if cfg.PostgresDSN == "" {
-		log.Println("[WARN] POSTGRES_DSN is empty, Postgres may not connect")
+	cfg := Config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalf("failed load env: %v", err)
 	}
 
 	return &cfg
---
+++ config/db.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"context"
+	"database/sql"
+	// "log"
+	"time"
+
+	_ "github.com/lib/pq"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectPostgres(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil { return nil, err }
+	db.SetMaxOpenConns(10)
+	db.SetConnMaxLifetime(time.Minute * 5)
+	if err := db.Ping(); err != nil { return nil, err }
+	return db, nil
+}
+
+func ConnectMongo(uri string) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil { return nil, err }
+	if err := client.Ping(ctx, nil); err != nil { return nil, err }
+	return client, nil
+}
